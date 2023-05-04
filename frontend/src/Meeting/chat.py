bd3c01ac47425bed837a750f5287fa131bb935fd
Krishna Gupta <gupta.krishna71@gmail.com>
2023-05-03 19:26:37 -0700

    socket chat functionality added (#6)

--- frontend/src/Meeting/chat.py
+++ frontend/src/Meeting/chat.py
@@ -1,23 +1,43 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLineEdit, QPushButton
-
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtWidgets import QApplication, QWidget, QListView, QVBoxLayout, QListWidget, QListWidgetItem, QLineEdit, QPushButton
+from PySide6.QtGui import QColor, QBrush
+import json
+from socket_client import SocketClient
+import time
 app = QApplication()
 
 
 # Prototype from chatgpt
 
 class ChatScreen(QWidget):
+
+    user_id = "1"
+    meeting_id = "1"
+    user_name = "krishna"
+
     def __init__(self):
         super().__init__()
+
+        self.socket_client = SocketClient()
+        self.socket_client.message_received.connect(self.receive_messages)
+
+        self.setWindowTitle("Chat")
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        # Create a QTextEdit for displaying the chat messages
-        self.chat_display = QTextEdit()
-        self.chat_display.setReadOnly(True)
+        # Create a QListWidget for displaying the chat messages
+        self.chat_display = QListWidget()
+        self.chat_display.setViewMode(QListView.ViewMode.ListMode)
         layout.addWidget(self.chat_display)
-
+        self.chat_display.setStyleSheet("""
+            QListWidget {
+                background-color: white;
+                border: 1px solid gray;
+                border-radius: 5px;
+                padding: 5px;
+            }
+        """)
         # Create a QLineEdit for entering new messages
         self.message_input = QLineEdit()
         layout.addWidget(self.message_input)
@@ -27,16 +47,54 @@ class ChatScreen(QWidget):
         layout.addWidget(self.send_button)
 
         # Connect the send_button clicked signal to the send_message method
-        self.send_button.clicked.connect(self.send_message)
+        self.send_button.clicked.connect(self.send_new_chat_message)
+        time.sleep(2)
+        if self.socket_client.get_connection_state():
+            print("Connected")
+            self.subscribeToChat()
+        else:
+            print("Not connected")
+        # self.chat_display.append(data)
 
-    def send_message(self):
-        # Get the message from the message_input and clear the input field
-        message = self.message_input.text()
+    def subscribeToChat(self):
+        subscriptionInfo = {"type": "getChatMessages",
+                            "meetingId": self.meeting_id, "userId": self.user_id}
+        self.socket_client.send_message(json.dumps(subscriptionInfo))
+
+    def send_new_chat_message(self):
+        messageText = self.message_input.text()
         self.message_input.clear()
+        newChatMessageText = {"type": "sendChatMessage",
+                              "meetingId": self.meeting_id, "userName": self.user_name, "message": messageText}
+        self.socket_client.send_message(json.dumps(newChatMessageText))
 
-        # Display the message in the chat_display
-        if message:
-            self.chat_display.append(message)
+    def receive_messages(self, data):
+        try:
+            if data["type"] == "chatMessageHistory":
+                self.loadChatMessagehistory(data["history"])
+            elif data["type"] == "newChatMessage":
+                self.addNewChatMessage(data["sender"], data["message"])
+        except Exception as e:
+            print(e)
+
+    def loadChatMessagehistory(self, chatHistory):
+        for message in chatHistory:
+            self.addNewChatMessage(message["sender"], message["message"])
+
+    def addNewChatMessage(self, username, message):
+        item = QListWidgetItem(f"{username}: {message}")
+        item.setTextAlignment(Qt.AlignRight if username ==
+                              self.user_name else Qt.AlignLeft)
+        item.setBackground(
+            QBrush(QColor("#0084ff") if username == self.user_name else QColor("#f2f2f2")))
+        item.setForeground(QBrush(Qt.white) if username ==
+                           self.user_name else QBrush(Qt.black))
+        sizeHint = QSize(item.sizeHint().width(), item.sizeHint().height()+5)
+        item.setSizeHint(sizeHint)
+
+        # Add the item to the QListWidget
+        self.chat_display.addItem(item)
+
 
 # Create a ChatScreen widget and show it
 chat_screen = ChatScreen()
