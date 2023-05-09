acf4bfdc69ce286716f23f5df21b488f8a6597e5
Pratyush Sharma <pratyushsharma@outlook.in>
2023-05-08 22:21:44 -0700

    Remove redundant parts and changing to QThread

--- frontend/src/Meeting/start_meeting_new.py
+++ frontend/src/Meeting/start_meeting_new.py
@@ -1,16 +1,10 @@
 from Meeting.meeting_page import MeetingPage
-from PySide6.QtWidgets import QMainWindow, QLabel
-from PySide6.QtCore import Qt, QSize, QTimer
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Qt, QSize
 from api_requests import end_meeting
-from Streaming.send_and_display_video_new import SendandDisplayVideo
-from Streaming.receive_stream import ReceiveStream
-import threading, time, cv2, qimage2ndarray, numpy, constant as const
-from ffpyplayer.player import MediaPlayer
-from Meeting.socket_client import SocketClient
+from Streaming.send_and_display_video import SendandDisplayVideo
+import constant as const
 from app_state import state
-
-user_stream_player : MediaPlayer = None
 
 class StartMeeting:
     def __init__(self, user_details, meeting_id) -> None:
@@ -25,33 +19,24 @@ class StartMeeting:
         self.mainWindow = QMainWindow()
         self.meeting_page.setupUi(self.mainWindow)
         self.meeting_page.end_meeting.clicked.connect(self.end_call)
-
-        self.send_stream = SendandDisplayVideo(self.meeting_page.self_video, self.meeting_id, self.user_id)
+        
+        self.thread_send_stream = SendandDisplayVideo(self.meeting_page.self_video, self.meeting_id, self.user_id)
         
         try:
-            # Send stream using ffmpeg and display it in UI
-            # thread_send_stream = threading.Thread(target=self.send_stream.send_stream_to_server, daemon=True)
-            # thread_send_stream.start()
-            self.send_stream.start()
+            self.thread_send_stream.start()
         except Exception as e:
             print("Exception occured while sending stream :", e)
         finally:
-            self.send_stream.stop()
+            self.thread_send_stream.stop()
 
         self.mainWindow.show()
-    
-    # Need to be called for each participant
-    def receive_video_of_participant(self):
-        receive_stream = ReceiveStream()
-        thread_show_stream = threading.Thread(target=receive_stream.start_participant_stream, args=(self.meeting_page.self_video, self.meeting_id, self.user_id), daemon=True)
-        thread_show_stream.start()
 
     def end_call(self):
         state.in_meeting = False
         try:
             self.meeting_page.socket_client.close_socket()
             # Stop sending and displaying own video
-            self.send_stream.stop()
+            self.thread_send_stream.stop()
             self.meeting_page.thread_show_stream.stop()
             end_meeting(self.user_id, self.meeting_id)
             self.mainWindow.close()
@@ -61,40 +46,3 @@ class StartMeeting:
             print("Exception occured during end meeting :", e)
         finally:
             self.mainWindow.close()
-
-    def start_participant_stream(self, user_video_tile : QLabel, meeting_id : str, user_id : str):
-        time.sleep(10)
-        url = f'{const.RTMP_URL}/{meeting_id}_{user_id}'
-
-        user_stream_player = MediaPlayer(url)
-        val =''
-
-        while(val != 'eof'):
-            frame, val = user_stream_player.get_frame()
-            if frame is not None:
-                img, t = frame
-                frame_data = numpy.frombuffer(img.to_bytearray()[0], dtype=numpy.uint8)
-                frame_data = frame_data.reshape((const.FRAME_HEIGHT, const.FRAME_WIDTH, 3))
-                frame_data = cv2.flip(frame_data, 1)
-                image = qimage2ndarray.array2qimage(frame_data)
-                pixmap = QPixmap.fromImage(image)
-                user_video_tile.setPixmap(pixmap)
-            elif frame is None:
-                time.sleep(0.01)
-
-        # def display_stream():
-        #     frame, val = user_stream_player.get_frame()
-        #     if val != "eof" and frame is not None:
-        #         img, t = frame
-        #         frame_data = numpy.frombuffer(img.to_bytearray()[0], dtype=numpy.uint8)
-        #         frame_data = frame_data.reshape((const.FRAME_HEIGHT, const.FRAME_WIDTH, 3))
-        #         frame_data = cv2.flip(frame_data, 1)
-        #         image = qimage2ndarray.array2qimage(frame_data)
-        #         pixmap = QPixmap.fromImage(image)
-        #         user_video_tile.setPixmap(pixmap)
-        #     elif frame is None:
-        #         time.sleep(0.01)
-
-        # self.timer = QTimer()
-        # self.timer.timeout.connect(display_stream)
-        # self.timer.start(30)
--- frontend/src/Streaming/send_and_display_video.py
+++ frontend/src/Streaming/send_and_display_video.py
@@ -1,10 +1,12 @@
+from PySide6.QtCore import QThread
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QLabel
 import cv2, subprocess, qimage2ndarray, time, constant as const
-import cv2, subprocess, qimage2ndarray, time, constant as const
 
-class SendandDisplayVideo():
+class SendandDisplayVideo(QThread):
+    
     def __init__(self, image_label : QLabel, meeting_id : str, user_id : str) -> None:
+        super().__init__()
         self.meeting_id = meeting_id
         self.user_id = user_id
         self.label = image_label
@@ -13,7 +15,7 @@ class SendandDisplayVideo():
 
         self.url = f'{const.RTMP_URL}/{self.meeting_id}_{self.user_id}'
 
-    def send_stream_to_server(self):
+    def run(self):
         send_command_opencv = ['ffmpeg', 
             '-f', 'rawvideo', # Take rawvideo as provided by opencv
             '-pix_fmt', 'bgr24', # Pix format of input video
@@ -56,9 +58,9 @@ class SendandDisplayVideo():
                 if ret:
                     #Display frame on Meeting Page
                     self.display_video_frame(frame)
+                    
                     # Write to open pipe in send command
                     self.send_process_opencv.stdin.write(frame.tobytes())
-
                     # Flush buffer data after it's sent
                     self.send_process_opencv.stdin.flush()
                 else:
@@ -81,77 +83,21 @@ class SendandDisplayVideo():
                 self.send_process_opencv.stdin.close()
             self.send_process_opencv.terminate()
 
-    def stop_stream(self):
-        self.close_called = True
-        if self.capture:
-            self.capture.release()
-        if self.send_process_opencv:
-            self.send_process_opencv.terminate()
-
     def display_video_frame(self, frame):
         # Convert the frame from BGR to RGB
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.flip(frame, 1)
 
         # Create a QImage from the frame data
-        # image = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
         image = qimage2ndarray.array2qimage(frame)
 
         # Create a QPixmap from the QImage
         pixmap = QPixmap.fromImage(image)
         self.label.setPixmap(pixmap)
 
-
-
-    # def send_stream_to_server_using_wrapper(self):
-    #     try:
-    #         capture = cv2.VideoCapture(cv2.CAP_V4L2) # cv2.CAP_V4L2 = 0. Default cam at 0 index for Linux. Need to set different values for each OS
-    #         capture.set(cv2.CAP_PROP_FRAME_WIDTH, const.FRAME_WIDTH)
-    #         capture.set(cv2.CAP_PROP_FRAME_HEIGHT, const.FRAME_HEIGHT)
-
-    #         video_input = ffmpeg.input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f'{const.FRAME_WIDTH}x{const.FRAME_HEIGHT}', thread_queue_size='1024')
-    #         audio_input = ffmpeg.input('default', format='alsa', channels='1', ar='44100', thread_queue_size='5000')
-    #         ouput = ffmpeg.output(video_input, audio_input, self.url, vcodec='libx264', acodec='aac', bufsize='500k', preset='veryfast', maxrate='3000k', tune='zerolatency', af='afftdn', format=f'{const.VIDEO_CODEC}')
-    #         self.process = ouput.run_async(pipe_stdin=True)
-
-    #         start_time = time.time()
-    #         tries = 0
-    #         self.close_called = False
-
-    #         while(True):
-    #             if self.close_called == True:
-    #                 break
-    #             ret, frame = capture.read()
-    #             if ret:
-    #                 #Display frame on Meeting Page
-    #                 # self.display_video_frame(frame)
-
-    #                 # Write to open pipe in send command and flush buffer after it's sent
-    #                 self.process.stdin.write(frame.tobytes())
-    #                 self.process.stdin.flush()
-    #             else:
-    #                 tries += 1
-    #                 if (tries >= const.MAX_TRIES and (time.time() - start_time) >= const.MAX_WAIT_TIME_FOR_SERVER): # Wait a maximum of wait time defined or max tries
-    #                     print("Frames not being sent after", const.MAX_TRIES, "tries. Closing operation")
-    #                     if capture:
-    #                         capture.release()
-    #                     if self.process:
-    #                         self.process.stdin.flush()
-    #                         self.process.stdin.close()
-    #                     self.process.terminate()
-    #                     return
-
-    #     except Exception as e:
-    #         print("Exception occured while sending/displaying self stream. :", e)
-    #     finally:
-    #         if capture:
-    #             capture.release()
-    #         if self.process:
-    #             self.process.terminate()
-
-    # def stop_stream_wrapper(self):
-    #     if self.process:
-    #         if self.process.stdin:
-    #             self.process.stdin.flush()
-    #             self.process.stdin.close()
-    #         self.process.terminate()
+    def stop(self):
+        self.close_called = True
+        if self.capture:
+            self.capture.release()
+        if self.send_process_opencv:
+            self.send_process_opencv.terminate()
