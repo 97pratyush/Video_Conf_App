b061e8019691576c4ba3a7ecb36383d7fcca05bc
Krishna Gupta <gupta.krishna71@gmail.com>
2023-05-08 19:59:16 -0700

    streaming bug fix (#12)

--- frontend/src/Streaming/receive_stream.py
+++ frontend/src/Streaming/receive_stream.py
@@ -1,23 +1,23 @@
-from PySide6.QtCore import QThread
+from PySide6.QtCore import QThread, Signal
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QLabel
 from ffpyplayer.player import MediaPlayer
 import cv2, qimage2ndarray, numpy, time, constant as const
 
-# class ReceiveStream(QThread):
-class ReceiveStream():
-    # def __init__(self, image_label : QLabel, url : str):
-    #     super().__init__()
-    #     self.image_label = image_label
-    #     self.url = url
+class ReceiveStream(QThread):
 
-    def start_participant_stream(self, user_video_tile : QLabel, meeting_id : str, user_id : str):
-        url = f'{const.RTMP_URL}/{meeting_id}_{user_id}'
+    frame_changed = Signal(object)
 
-        user_stream_player = MediaPlayer(url)
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
 
-        while(True):
-            frame, val = user_stream_player.get_frame()
+    def run(self):
+        self._running = True
+        player = MediaPlayer(self.url)
+
+        while(self._running):
+            frame, val = player.get_frame()
             if val != "eof" and frame is not None:
                 img, t = frame
                 frame_data = numpy.frombuffer(img.to_bytearray()[0], dtype=numpy.uint8)
@@ -25,6 +25,11 @@ class ReceiveStream():
                 frame_data = cv2.flip(frame_data, 1)
                 image = qimage2ndarray.array2qimage(frame_data)
                 pixmap = QPixmap.fromImage(image)
-                user_video_tile.setPixmap(pixmap)
+                self.frame_changed.emit(pixmap)
             elif frame is None:
                 time.sleep(0.01)
+
+
+    def stop(self):
+        self._running = False
+        self.wait()
