dd5f65d5ab83227b2fb7fb2a21733bf04930acf6
Krishna Gupta <gupta.krishna71@gmail.com>
2023-05-12 18:59:18 -0700

    endpoint updated (#17)

--- frontend/src/constant.py
+++ frontend/src/constant.py
@@ -18,10 +18,10 @@ MAX_WAIT_TIME_TO_SEND = 5
 MAX_TRIES = 30
 
 # Define the address and port to use for the socket connection
-WS_URL = "ws://2.tcp.ngrok.io:14936"
+WS_URL = "ws://6.tcp.ngrok.io:13939"
 
 # API URL
-URL = "https://6f0c-2601-646-9d01-1da0-4a80-d63-becb-76c2.ngrok-free.app"
+URL = "https://0254-2601-642-4c05-56b2-ebe4-97a9-2807-917d.ngrok-free.app"
 
 # Socket Constants - Meeting Participants
 PARTICIPANTS_TOPIC = 'subscribeToParticipantList'
